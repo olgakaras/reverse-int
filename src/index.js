32fb6949a8ebcb8593d36455c116752d28b0e547
olgakaras <olga.karas123@gmail.com>
2022-01-09 21:42:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) { 
+  if (n < 0) {
+    n = n * (-1);
+  }
+  let arrNum = Array.from(String(n), Number);
+  let newArr = [];
+  for(let i = arrNum.length - 1; i > -1; i--) {
+      newArr = newArr + arrNum[i];
+  }
+  let signNumber = n < 0 ? -Number(newArr) : Number(newArr);
+  return signNumber;
 }
